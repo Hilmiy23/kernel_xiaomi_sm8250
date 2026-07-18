8a9579876a55572ab78d102946903b126b540640
@Moriset <hilmizh23@gmail.com>
2026-07-18 23:52:36 +0700

    Revert "drivers/usb/typec: Checkout to LineageOS kernel"
    This reverts commit e95de3e7cc253b17a7eaafb41cdc387b197deb4c.

--- drivers/usb/typec/class.c
+++ drivers/usb/typec/class.c
@@ -3,6 +3,7 @@
  * USB Type-C Connector Class
  *
  * Copyright (C) 2017, Intel Corporation
+ * Copyright (C) 2021 XiaoMi, Inc.
  * Author: Heikki Krogerus <heikki.krogerus@linux.intel.com>
  */
 
@@ -189,13 +190,11 @@ static void typec_altmode_put_partner(struct altmode *altmode)
 {
 	struct altmode *partner = altmode->partner;
 	struct typec_altmode *adev;
-	struct typec_altmode *partner_adev;
 
 	if (!partner)
 		return;
 
-	adev = &altmode->adev;
-	partner_adev = &partner->adev;
+	adev = &partner->adev;
 
 	if (is_typec_plug(adev->dev.parent)) {
 		struct typec_plug *plug = to_typec_plug(adev->dev.parent);
@@ -204,7 +203,7 @@ static void typec_altmode_put_partner(struct altmode *altmode)
 	} else {
 		partner->partner = NULL;
 	}
-	put_device(&partner_adev->dev);
+	put_device(&adev->dev);
 }
 
 static int typec_port_fwnode_match(struct device *dev, const void *fwnode)
@@ -479,11 +478,9 @@ static void typec_altmode_release(struct device *dev)
 {
 	struct altmode *alt = to_altmode(to_typec_altmode(dev));
 
-	if (!is_typec_port(dev->parent))
-		typec_altmode_put_partner(alt);
+	typec_altmode_put_partner(alt);
 
 	altmode_id_remove(alt->adev.dev.parent, alt->id);
-	put_device(alt->adev.dev.parent);
 	kfree(alt);
 }
 
@@ -532,8 +529,6 @@ typec_register_altmode(struct device *parent,
 	alt->adev.dev.groups = alt->groups;
 	alt->adev.dev.type = &typec_altmode_dev_type;
 	dev_set_name(&alt->adev.dev, "%s.%u", dev_name(parent), id);
-
-	get_device(alt->adev.dev.parent);
 
 	/* Link partners and plugs with the ports */
 	if (is_port)
@@ -1390,6 +1385,7 @@ void typec_set_pwr_opmode(struct typec_port *port,
 						(opmode > TYPEC_PWR_MODE_MAX))
 		return;
 
+	pr_err("%s pwr opmode:%d\n", __func__, opmode);
 	if (opmode > TYPEC_PWR_MODE_PD)
 		return;
 
